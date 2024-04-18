8ac32210ca071933dba28f6e2986341031bfb463
acartiermichaud <a.cartier.michaud@gmail.com>
2024-04-18 10:23:42 +0200

    Responsive

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,3 +1,6 @@
+// React
+import {useState} from 'react'
+
 // PropTypes
 import PropTypes from 'prop-types'
 
@@ -12,6 +15,18 @@ import logo from '../../images/logo.png'
 
 
 function Header ({activeTab}) {
+
+  const [displayMenu, setDisplayMenu] = useState(false)
+  console.log("displayMenu : "+displayMenu)
+
+  function openMenu () {
+    if (displayMenu === false) {
+      setDisplayMenu(true)
+    }
+    else {
+      setDisplayMenu(false)
+    }
+  }
   
   return (
     <header className="header">
@@ -20,10 +35,11 @@ function Header ({activeTab}) {
           <img src={logo} alt="logo" className="header_logo" loading="lasy"/>
             <div className="header_title">
               <h1>Alain Cartier-Michaud</h1>
-              <p>Hypnothérapeute | Énergéticien | Facilitateur PSYCH-K</p>
+              <p>Hypnothérapeute | Énergéticien <span className='header_title_separator'>|</span> Facilitateur PSYCH-K</p>
             </div>
         </div>
-        <nav className='header_nav'>
+        <button className='header_button' onClick={() => openMenu()}><i className="header_menu-icon fa-solid fa-bars"></i></button>
+        <nav className={displayMenu === false ? 'header_nav' : "header_mobile-nav"}>
           <Tab isActive={activeTab==="home"} path="/" text="Accueil"/>
           <Tab isActive={activeTab==="practices"} path="/pratiques-et-tarifs" text="Pratiques & Tarifs"/>
           <Tab isActive={activeTab==="about"} path="/a-propos-de-moi" text="A propos de moi"/>
